ae4fdbf6e0961b60a44525aea88c6e00b1ec9894
Josh Thompson <thompsonjoshd@gmail.com>
2017-04-09 16:28:20 -0600

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Category.destroy_all
+GifCategory.destroy_all
+FavoriteGif.destroy_all
+Gif.destroy_all
+User.destroy_all
 
-Gif.create(image_path: 'https://media.giphy.com/media/1apNICVnC3AEo/giphy.gif')
-Gif.create(image_path: 'https://media.giphy.com/media/yYSSBtDgbbRzq/giphy.gif')
+bunny = Category.find_or_create_by(name: "bunny")
+bunny.gifs.find_or_create_by(image_path: "https://media.giphy.com/media/1apNICVnC3AEo/giphy.gif")
+bunny.gifs.find_or_create_by(image_path: "https://media.giphy.com/media/ef61oIGVyckY8/giphy.gif")
+bunny.gifs.find_or_create_by(image_path: "https://media.giphy.com/media/MZzMRR6JfIiME/giphy.gif")
+
+truck = Category.find_or_create_by(name: "truck")
+truck.gifs.find_or_create_by(image_path: "https://media.giphy.com/media/VTLkM7Z0a2I48/giphy.gif")
+truck.gifs.find_or_create_by(image_path: "https://media.giphy.com/media/RnJhBjGHJPos/giphy.gif")
+truck.gifs.find_or_create_by(image_path: "https://media.giphy.com/media/G8EWOKSMUv7AA/giphy.gif")
+
+User.find_or_create_by(first_name: "Josh", last_name: "Thompson",
+            email: "example@example.com",
+            password_digest: "password", password_confirmation: "password",
+            user_role: 0)
+User.find_or_create_by(first_name: "Sean", last_name: "Shawnson",
+            email: "example@example.com",
+            password_digest: "password", password_confirmation: "password",
+            user_role: 0)
+paul = User.find_or_create_by(first_name: "Paul", last_name: "Paulson",
+            email: "example@example.com",
+            password_digest: "password", password_confirmation: "password",
+            user_role: 0)
+paul.favorite_gifs.find_or_create_by(gif: truck.gifs.last)
+paul.favorite_gifs.find_or_create_by(gif: bunny.gifs.last)
+
+p "Sample of what was created: #{Category.first}, #{Gif.first}, #{User.first}"
